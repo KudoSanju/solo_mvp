57186dbce4c47719e18ab4e9e6b45aa1085fbc7a
Kudo Sanju <Noelle07072020@gmail.com>
2022-11-10 18:06:29 +0900

    chore: cleanup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const knex = require("../db/knex");
+// const db =require('../db/knex');
  
 function setupServer() {
   const app = express(); //initializing server
 
-  app.use(express.json())
+  app.use(express.json());
+
   app.get("/api/test",(req,res) => {
     res.status(200).send("Hello");
+  });
+
+  
+
+  app.get("/api/outfits", (req,res) => {
+    const outfits = knex.select('*').from(outfits) 
+    res.status(200).send(outfits);
+    // try{
+    //   const outfits = await knex ('outfits')
+    //   .select('*')
+    //   .timeout(1500);
+    //   outfits.length > 0
+   
+    //   :res.status(404).send('no outfits found');
+      
+    // }catch(err) {
+    //   res.status(500).send(err)
+    // }
+    
   })
   return app;
 }
