e5cc7ef73941a4692aa8cb6a0b1cdd7a90111505
Kudo Sanju <Noelle07072020@gmail.com>
2022-11-16 22:20:25 +0900

    refactor: update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  development:{
-    client: "pg",
-    connection: process.env.DB_URL_DEV, 
-    searchPath: 'public',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: __dirname + '/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/db/seeds'
-    }
-  },
+  
   production:{
     client: "pg",
     connection: process.env.DB_URL_PROD, 
