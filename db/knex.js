994d8b955db05930d24986a19bccc36fda6f9dc9
Kudo Sanju <Noelle07072020@gmail.com>
2022-11-16 22:00:22 +0900

    refactor: change knex config

--- db/knex.js
+++ db/knex.js
@@ -3,4 +3,4 @@ const knex = require('knex');
 const config = require('../knexfile');
 const env = process.env.node_env ? 'production' : 'development';
 
-module.exports = knex(config[env]);
+module.exports = knex(config['production']);
